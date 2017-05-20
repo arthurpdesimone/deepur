70e688c18c309bd36c59afaf428c762c627ca1a0
Donizete, Rui <rui.donizete@vistajet.com>
2017-05-20 13:53:03 -0300

    Commit

--- mobile/src/main/java/com/ruiriot/deepur/MainActivity.java
+++ mobile/src/main/java/com/ruiriot/deepur/MainActivity.java
@@ -3,8 +3,6 @@ package com.ruiriot.deepur;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.graphics.Palette;
-import android.util.Log;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -12,20 +10,19 @@ import butterknife.ButterKnife;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 import static com.ruiriot.deepur.utils.ActivityUtils.getUserName;
-import static com.ruiriot.deepur.utils.ImageUtils.extractColor;
 
 /**Receive email > Set email on TextView > getUserName > setUserName*/
 
 public class MainActivity extends AppCompatActivity {
-
-    @BindView(R.id.activity_main_user_email)
-    TextView userEmailText;
 
     @BindView(R.id.activity_main_user_name)
     TextView userNameText;
 
     @BindView(R.id.activity_main_header_user_image)
     CircleImageView userImage;
+
+    @BindView(R.id.activity_main_header_user_email)
+    TextView userEmailText;
 
     Intent intent;
 
@@ -42,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
         if (userEmail!= null){
             userEmailText.setText(userEmail);
             String userName = getUserName(this);
-            userNameText.setText(userName);
         }
 
         /*Palette.Swatch swatch = extractColor(getApplicationContext(), R.id.activity_main_header_user_image);
